42e652aacd3cce0d843c255037eb86616c1e22a3
Sadriddin Akhmadullayev <mrakhmadullayev@Sadriddins-MacBook-Pro.local>
2026-06-04 23:07:21 +0500

    Update api

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001',
-  },
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://34.229.59.215:5000/api/:path*',
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
